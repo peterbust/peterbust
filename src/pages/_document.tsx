2c82b8ef99bbf6438d79b5db97895c394e937d20
peterbust <peterbust@me.com>
2022-02-27 14:11:02 +0100

    Lighthouse optimizations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,14 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="robots" content="noindex" />
+          <meta
+            name="description"
+            content="Senior front-end developer favoring React"
+          />
         </Head>
         <body>
           <Main />
